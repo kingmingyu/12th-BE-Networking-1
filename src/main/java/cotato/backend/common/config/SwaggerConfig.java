fa347e343dfc7e8f07f0a593da1921b10e196e64
김민규 <alsrb0332@naver.com>
2026-05-25 09:08:01 +0900

    feat: Swagger 개발 서버 주소 변경(.nip.io & https 설정)

--- src/main/java/cotato/backend/common/config/SwaggerConfig.java
+++ src/main/java/cotato/backend/common/config/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(info)
                 .addServersItem(new Server().url("http://localhost:8080").description("로컬"))
-                .addServersItem(new Server().url("http://43.200.76.50:8080").description("개발 서버"))
+                .addServersItem(new Server().url("https://api.43.200.76.50.nip.io").description("개발 서버"))
                 .addSecurityItem(securityRequirement)
                 .components(components);
     }
